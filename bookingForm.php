cbcb413e3f797ad31c35766db5fa2d48608239c8
mfarzamalam <farzualam9999@gmail.com>
2020-07-04 21:04:03 +0500

    Booking form and searching is completed

---
+++ bookingForm.php
@@ -0,0 +1,147 @@
+<?php include 'connection.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colrolib Templates">
+    <meta name="author" content="Colrolib">
+    <meta name="keywords" content="Colrolib Templates">
+
+    <!-- Title Page-->
+    <title>Au Form Wizard</title>
+
+    <?php include 'headerlink.php'; ?>
+
+
+    <link href="http://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+    <link href="http://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js">
+      
+
+    <!-- Main CSS-->
+    <link href="css/gAdd.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css">
+
+    <link href="css/booking.css?c=<?php echo time(); ?>" rel="stylesheet" media="all">
+
+</head>
+
+<body>
+
+    <?php include 'header.php'; 
+
+
+//     if(isset($_GET['search'])){
+//         echo "<h1>$_GET[date]</h1>";
+//         echo "<h1>$_GET[range]</h1>";
+
+//     if($_GET['Day'] == "Day" && $_GET['Night'] == "Night"){
+        
+//         echo "<h1>Day and night</h1>";
+
+//     } 
+//     else if ($_GET['Day'] == "Day" && $_GET['Night'] == ""){
+                
+//         echo "<h1>Day</h1>";
+
+//     }
+//     else if ($_GET['Night'] == "Night" && $_GET['Day'] == ""){
+//         echo "<h1>Night</h1>";
+//     }
+// }
+
+    // while ($loop = mysqli_fetch_assoc($r)){
+    
+    
+    ?>
+<!-- 
+    <table border="1">
+        <tr>
+            <th>ground name </th>
+            <th>ground description </th>
+            <th>ground owner </th>
+            <th>ground owner number </th>
+            <th>ground rent day </th>
+            <th>ground rent night </th>
+            <th>ground day </th>
+            <th>ground night </th>
+            <th>ground available </th>
+        </tr>
+        <tr>
+            <td><?php echo $loop['ground_name']; ?></td>
+            <td><?php echo $loop['ground_des']; ?></td>
+            <td><?php echo $loop['ground_owner']; ?></td>
+            <td><?php echo $loop['ground_owner_num']; ?></td>
+            <td><?php echo $loop['rent_day']; ?></td>
+            <td><?php echo $loop['rent_night']; ?></td>
+            <td><?php echo $loop['Day']; ?></td>
+            <td><?php echo $loop['Night']; ?></td>
+            <td><?php echo $loop['available']; ?></td>
+        </tr>
+    </table> -->
+
+ <!-- while loop tag close here -->
+
+    <div class="styling">
+        <div class="page-wrapper bg-img-1 p-t-165 p-b-100">
+            <div class="wrapper wrapper--w720">
+                <div class="card card-3">
+                    <div class="card-body">
+                        <ul class="tab-list">
+                            <li class="tab-list__item active">
+                                <a class="tab-list__link" href="#tab1" data-toggle="tab">hotels</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab1">
+                                <form method="POST" action="bookingSearch.php">
+                                    <div class="input-group">
+                                        <label class="label">Select date</label>
+                                        <input class="is1" type="date" name="date">
+                                        <i class="zmdi zmdi-pin input-group-symbol"></i>
+                                    </div>
+                                    <div class="input-group">
+                                        <label class="label">Range Maximum</label>
+                                        <input class="is1" name="range" type="range" min="1000" max="10000" value="1000" id="myRange">
+                                        <i class="zmdi zmdi-pin input-group-symbol"></i>
+                                        <p>Value (rs): <span id="demo"></span></p>
+                                    </div>
+
+                                    <div class="checkbox-row">
+                                        <label class="checkbox-container m-r-45">Day
+                                            <input type="checkbox" name='Day' value="Day">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        <label class="checkbox-container">Night
+                                            <input type="checkbox" name='Night' value="Night">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                    <button class="btn-submit" type="submit" name="search">search</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    </div>
+
+<script>
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+</script>
+
+ 
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
+<!-- end document-->
---
+++ bookingSearch.php
@@ -0,0 +1,94 @@
+<?php include 'connection.php'; 
+
+    if(isset($_POST['search'])){        
+        $range = $_POST['range'];
+        $Day = $_POST['Day'];
+        $Night = $_POST['Night'];
+        $date = $_POST['date'];
+
+        if($Day == "Day" && $Night == "Night"){
+
+            // (From_date <= '2013-01-03' AND To_date >= '2013-01-09')
+            $q = "SELECT * FROM `grounds` WHERE `rent_day` LIKE 'Yes' 
+                                            AND `rent_night` LIKE 'Yes' 
+                                            AND `available` LIKE 'Yes'
+                                            AND `Day` BETWEEN 1000 AND '$range'
+                                            AND `Night` BETWEEN 1000 AND '$range'";
+            $r = mysqli_query($conn,$q);
+
+        } 
+        else if ($Day == "Day" && $Night == ""){
+            $q = "SELECT * FROM `grounds` WHERE `rent_day` LIKE 'Yes' 
+                                            AND `available` LIKE 'Yes'
+                                            AND `Day` BETWEEN 1000 AND '$range'";
+            $r = mysqli_query($conn,$q);
+        
+        }
+        else if ($Night == "Night" && $Day == ""){
+            $q = "SELECT * FROM `grounds` WHERE `rent_day` LIKE 'Yes' 
+                                            AND `available` LIKE 'Yes'
+                                            AND `Night` BETWEEN 1000 AND '$range'";
+            $r = mysqli_query($conn,$q);
+        
+        }
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Table V04</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include 'headerlink.php'; ?>
+
+	<link rel="stylesheet" type="text/css" href="css/bookingSearch.css">
+
+</head>
+<body>
+    
+    <?php include 'header.php'; ?>
+
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100 ver1 m-b-110">
+					<div class="table100-head">
+						<table>
+							<thead>
+								<tr class="row100 head">
+									<th class="cell100 column1">Ground name</th>
+									<th class="cell100 ">Club it's belong</th>
+									<th class="cell100 ">Ground Owner</th>
+									<th class="cell100 ">Available</th>
+                                    <th class="cell100 ">Sign up now</th>
+                                    
+								</tr>
+							</thead>
+						</table>
+					</div>
+
+					<?php while($loop = mysqli_fetch_assoc($r)){ ?>
+						<div class="table100-body js-pscroll">
+							<table>
+								<tbody>
+									<tr class="row100 body">
+										<td class="cell100 column1"><?php echo $loop['ground_name']; ?></td>
+										<td class="cell100 "><?php echo $loop['club_code']; ?></td>
+										<td class="cell100 "><?php echo $loop['ground_owner']; ?></td>
+										<td class="cell100 "><?php echo $loop['available']; ?></td>
+										<td><a href="bookingRegister.php?id=<?php echo $loop['ground_code']; ?>"><button class="btn btn--radius-2 btn--red">Register</button></a></td>                          
+									</tr>
+
+								</tbody>
+							</table>
+						</div>
+			    	<?php } ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
